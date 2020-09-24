92eac668d7057b90a72c74e13591300457b20a65
PrisionMike <su.sh2396@gmail.com>
2020-09-24 21:20:45 +0530

    major. Much awaited. Font change.

--- test_textinput.py
+++ test_textinput.py
@@ -5,37 +5,42 @@ class Tweewin(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.shouldtweet = False
+        self.siztup = (800,500)
+        self.buttonleveloff = 100
         self.winsetup()
 
     def winsetup(self):
 
         self.setWindowTitle("Tweeter pop-up")
-        self.resize(400,300)
+        self.resize(self.siztup[0],self.siztup[1])
         self.center()
+        self.textfont = QtGui.QFont("Times",14)
 
         tweetbtn = QtWidgets.QPushButton('Tweet',self)
         tweetbtn.resize(tweetbtn.sizeHint())
-        tweetbtn.move(50,200)
+        tweetbtn.move(self.buttonleveloff,self.siztup[1] - self.buttonleveloff)
         tweetbtn.clicked.connect(self.tweetit)
 
         cancelbtn = QtWidgets.QPushButton('Cancel', self)
-        cancelbtn.move(200,200)
+        cancelbtn.move(4*self.buttonleveloff,self.siztup[1] - self.buttonleveloff)
         # cancelbtn.clicked.connect(QtWidgets.QApplication.instance().quit)
         cancelbtn.clicked.connect(self.close)
         cancelbtn.resize(cancelbtn.sizeHint())
 
         self.tweettext = ""
-        self.txtbox = QtWidgets.QLineEdit(self)
-        self.txtbox.returnPressed.connect(self.tweetit)
+        # self.txtbox = QtWidgets.QLineEdit(self)
+        self.txtbox = QtWidgets.QPlainTextEdit(self)
+        # self.txtbox.returnPressed.connect(self.tweetit)
+        self.txtbox.setFont(self.textfont)
         self.txtbox.setFocus()
         self.txtbox.move(10,10)
-        self.txtbox.resize(350,100)
+        self.txtbox.resize(self.siztup[0]-20,self.siztup[1]/2)
 
         # self.tweettxt,self.status = QtWidgets.QInputDialog.getText(self,'input dialog','haal kaisa hai janab ka?')
         self.show()
     
     def tweetit(self):
-        self.tweettext = self.txtbox.text()
+        self.tweettext = self.txtbox.toPlainText()
         if self.tweettext != "":
             self.shouldtweet = True
         # print('the received tweet is',self.tweettext)
