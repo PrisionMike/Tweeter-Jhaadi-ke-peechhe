9a898c86c1bb96442c8647497a1f69253a5af146
PrisionMike <su.sh2396@gmail.com>
2020-10-25 20:01:39 +0530

    main file updated

--- main2.py
+++ main2.py
@@ -1,9 +1,11 @@
 # Our new main file. Being smart. Using the tweeting part as a procedure. not the GUI part.
 
 from textotweet import tweetthetext
+from guiclass import Newmainwin
+from PyQt5 import QtWidgets
 
-pass
-
-# g = input("Ssup loser?\n")
-# tweetthetext(g)
+app = QtWidgets.QApplication(sys.argv)
+twiwin = Newmainwin()
+twiwin.show()
+app.exec_()
 
